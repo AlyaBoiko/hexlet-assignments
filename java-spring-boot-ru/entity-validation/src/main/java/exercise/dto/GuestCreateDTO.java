1b77d50c20ac8d7324ed9a17e5332a4f3de2a7d6
@hexlet/cli <support@hexlet.io>
2025-05-14 17:51:21 +0300

    submit java-spring-boot-ru/entity-validation

---
+++ java-spring-boot-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -0,0 +1,29 @@
+package exercise.dto;
+
+// BEGIN
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class GuestCreateDTO {
+    @NotNull
+    private String name;
+
+    @Email
+    private String email;
+
+    @Size(min = 11, max = 13, message = "Введен невалидный номер, он должен содержать не менее 11 и не более 13 символов")
+    @Pattern(regexp = "^\\+\\d+$", message = "Номер телефона должен начинаться с '+'")
+    private String phoneNumber;
+
+    @Size(min = 4, max = 4)
+    private String clubCard;
+
+    @FutureOrPresent(message = "Срок действия клубной карты должен быть в будущем или сегодня.")
+    private LocalDate cardValidUntil;
+}
+// END
---
+++ java-spring-boot-ru/entity-validation/src/main/java/exercise/model/Guest.java
@@ -0,0 +1,36 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "guests")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String clubCard;
+
+    private LocalDate cardValidUntil;
+
+    @CreatedDate
+    private LocalDate createdAt;
+}
